6f6abc3748933d1ff28e5e84dbf23fec37a561d8
Ronaldo Aquino <ronaldoaquino234@gmail.com>
2021-11-24 14:13:39 -0300

    Adicionando conteudo de string

---
+++ ProgramacaoPythonIniciante/manipulacaoDeVariaveisEStrings.py
@@ -0,0 +1,29 @@
+# Variaveis inteiras
+numero = -3
+numero_jogos = 14
+numero_convidados = 15
+
+# print(numero)
+# print(numero, numero_convidados)
+
+
+# Variaveis ponto flutuante
+pi = 3.14
+numero_euler = 2.71
+escala_terremoto = -4.55
+
+# print(pi)
+# print(numero_euler)
+# print(escala_terremoto)
+
+
+# Variaveis strings e chars
+letra = 'a'
+palavra1 = 'linguagem de programação'
+palavra2 = 'Python'
+
+# print(letra, palavra1)
+# print('Esta', palavra1, 'se chama', palavra2)
+
+idade = input("Digite sua idade:")
+print("Você tem", idade, "anos.")
