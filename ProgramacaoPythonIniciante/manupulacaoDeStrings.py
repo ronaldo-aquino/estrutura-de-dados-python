35dceb52a0c514db748edc64baeec9caf5490c3b
Ronaldo Aquino <ronaldoaquino234@gmail.com>
2021-11-25 12:10:10 -0300

    Aula sobre manipulacao de strings

---
+++ ProgramacaoPythonIniciante/manupulacaoDeStrings.py
@@ -0,0 +1,37 @@
+a =  "casaco"
+# print(a)
+
+maiusculo = a.upper()
+# print(maiusculo)
+
+minusculo = maiusculo.lower()
+# print(minusculo)
+
+capital = a.capitalize()
+# print(capital)
+
+metade_palavra = a[0: 3]
+# print(metade_palavra)
+
+ultimas_letras = a[3:]
+# print(ultimas_letras)
+
+b = a.replace('aco', 'inha')
+# print(a)
+# print(b)
+
+c = a.replace('o', 'a')
+# print(c)
+
+encontrar = c.find('s') 
+# print(encontrar)
+
+e = ' casaco '
+# print(len(e))
+
+f = e.strip()
+# print(f)
+
+n1 = 14
+n2 = 16
+print(f'{n1} dividido por {n2} = {n1/n2}')
