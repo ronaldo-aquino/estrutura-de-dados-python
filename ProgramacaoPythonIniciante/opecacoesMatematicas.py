a86a2e310c52e33c0033b7e8730ab0b0ceba95bd
Ronaldo Aquino <ronaldoaquino234@gmail.com>
2021-11-25 12:29:23 -0300

    Operadores matematicos

---
+++ ProgramacaoPythonIniciante/opecacoesMatematicas.py
@@ -0,0 +1,15 @@
+import math
+
+a = 5
+b = 3
+# print(a, b)
+# print(f"A soma de {a} + {b} = {a+b}")
+# print(f'{a} elevado a {3} = {a**b}')
+
+raiz_quadrada = math.sqrt(81)
+# print(raiz_quadrada)
+
+casos_doenca = 134
+numero_habitantes = 34432
+casos_por_habitantes = casos_doenca / numero_habitantes
+print(round(casos_por_habitantes, 4))
