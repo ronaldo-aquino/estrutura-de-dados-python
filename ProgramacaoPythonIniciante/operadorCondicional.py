7eb7962056c1e04fe336fa56a1293e456d21420e
Ronaldo Aquino <ronaldoaquinodev@gmail.com>
2021-11-27 13:09:53 -0300

    Aula sobre operador condicional

---
+++ ProgramacaoPythonIniciante/operadorCondicional.py
@@ -0,0 +1,28 @@
+x = 5
+y = 3
+
+# if x > y:
+#     print(f"{x} é maior que {y}")
+
+# if x > y:
+#     print(f"{x} é maior")
+# else:
+#     print(f"{x} é menor")
+
+# n = 4
+
+# if n == 4:
+#     print(f"{n} é igual a 4")
+# else:
+#     if n == 3:
+#         print(f"{n} é igual a 3")
+#     else:
+#         print(f"{n} não é igual a 3 nem 4")
+
+n1 = 2
+n2 = 2
+
+if(n1 > 1) and (n2 % 2 == 0):
+    print(f"{n1} é maior que 1 e {n2} é par")
+else:
+    print("Uma ou nenhuma das condições foram satisfeitas")
