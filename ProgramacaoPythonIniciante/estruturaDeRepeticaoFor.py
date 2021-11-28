5b3d46b6cfb860955041cb841eca41a7057adc57
Ronaldo Aquino <ronaldoaquinodev@gmail.com>
2021-11-28 15:53:31 -0300

    Trabalahndo com estrutura de repeticao for

---
+++ ProgramacaoPythonIniciante/estruturaDeRepeticaoFor.py
@@ -0,0 +1,32 @@
+# print(1)
+# print(2)
+# print(3)
+# print(4)
+# print(5)
+
+# for numero in range(1, 11):
+#     print(numero)
+
+# for numero in range(5, 0, -1):
+#     print(numero)
+
+# soma = 0
+
+# for numero in range(1, 6):
+#     soma += numero
+
+# print(soma)
+
+# palavra = 'sorvete'
+
+# for letra in palavra:
+#     # print(letra)
+#     if letra == 'e':
+#         print(letra)
+
+for i in range(0, 5):
+    print(i)
+    print('-----')
+    for j in range(0, 3):
+        print(j)
+    print()
