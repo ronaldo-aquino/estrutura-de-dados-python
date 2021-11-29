29da3995bc375b8fde69da4f5027c0cf69a39eca
Ronaldo Aquino <ronaldoaquino234@gmail.com>
2021-11-29 14:30:09 -0300

    Aula sobre tuplas e listas

---
+++ tuplasListas.py
@@ -0,0 +1,22 @@
+tupla = ('Homo sapiens', 'Canis familiaris', 'Felis catus')
+# print(tupla[2])
+
+# print(tupla.index('Canis familiaris'))
+
+# for elemento in tupla:
+#     print(elemento)
+
+l1 = ['Homo sapiens', 'Canis familiaris', 'Felis catus']
+l2 = ['Xenopus laevis', 'Ailuropoda melanoleuca']
+
+l3 = l1 + l2
+
+# print(l3)
+# print(l1[0:2])
+
+l1.append("Gorila gorila")
+# print(l1)
+
+l1.remove('Felis catus')
+del(l1)
+# print(l1)
