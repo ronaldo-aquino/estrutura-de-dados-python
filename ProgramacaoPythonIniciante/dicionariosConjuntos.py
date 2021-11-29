de4c4bf557caaf9c600f398498d6da9281db7296
Ronaldo Aquino <ronaldoaquino234@gmail.com>
2021-11-29 15:07:36 -0300

    Dicionarios e conjuntos

---
+++ ProgramacaoPythonIniciante/dicionariosConjuntos.py
@@ -0,0 +1,9 @@
+coleta = {"Tuberculose": 32, "Câncer": 55, "Diabetes": 150}
+
+# print(coleta['Tuberculose'])
+
+coleta["Pressão alta"] = 159
+
+# print(coleta['Pressão alta'])
+
+orgraos = ("Coração", "Rins", "Pulmão")
