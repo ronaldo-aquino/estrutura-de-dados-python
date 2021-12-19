7cc300970cf6ee3c24727e8aeced6350f68195a3
Ronaldo Aquino <ronaldoaquinodev@gmail.com>
2021-12-19 12:03:01 -0300

    Trabalhando com as bibliotecas math e datetime

---
+++ ProgramacaoPythonIniciante/modulosUteis.py
@@ -0,0 +1,32 @@
+# import math
+
+# print(math.sqrt(9))
+
+# print(math.sin(45))
+
+# print(math.cos(45))
+
+# print(math.log(1000, 10))
+
+# print(math.log(32, 2))
+
+# print(math.log(1000))
+
+# print(math.pi)
+
+import datetime
+
+# print(dir(datetime))
+
+# print(datetime.date.today())
+
+datetime.datetime.now()
+
+data = datetime.date(2020, 7, 10)
+
+# print(data.day)
+# print(data.month)
+# print(data.year)
+
+print(f"{data.day} do {data.month} de {data.year}")
+
