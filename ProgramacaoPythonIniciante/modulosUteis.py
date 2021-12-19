e0f6b2508f49b35de215fbd6d14da7b58ce801fa
Ronaldo Aquino <ronaldoaquinodev@gmail.com>
2021-12-19 12:12:31 -0300

    Trabalhando com as bibliotecas random e time

--- ProgramacaoPythonIniciante/modulosUteis.py
+++ ProgramacaoPythonIniciante/modulosUteis.py
@@ -20,13 +20,41 @@ import datetime
 
 # print(datetime.date.today())
 
-datetime.datetime.now()
+# datetime.datetime.now()
 
-data = datetime.date(2020, 7, 10)
+# data = datetime.date(2020, 7, 10)
 
 # print(data.day)
 # print(data.month)
 # print(data.year)
 
-print(f"{data.day} do {data.month} de {data.year}")
+# print(f"{data.day} do {data.month} de {data.year}")
 
+import random
+
+# print(random.random())
+
+# print(random.randint(1, 10))
+
+# print(random.randrange(0, 10, 2))
+
+# print(random.randrange(0, 10, 3))
+
+x = ['k', 'd', 12, '34-j', 'x']
+
+# print(random.choice(x))
+
+import time as tm 
+
+# print(tm.time())
+
+antes = tm.time()
+
+lista = [];
+
+for i in range(0, 100000):
+  lista.append(i)
+  
+depois = tm.time()
+
+print(depois - antes)
